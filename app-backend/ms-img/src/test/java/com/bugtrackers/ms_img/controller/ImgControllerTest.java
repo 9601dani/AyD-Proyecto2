014a66020196251a16cbbf0f2c3c70b300f56494
Leví Hernández <levihernandez2501@gmail.com>
2024-10-11 18:57:29 -0600

    Fix: add file property in test and prod profiles

--- app-backend/ms-img/src/test/java/com/bugtrackers/ms_img/controller/ImgControllerTest.java
+++ app-backend/ms-img/src/test/java/com/bugtrackers/ms_img/controller/ImgControllerTest.java
@@ -2,27 +2,18 @@ package com.bugtrackers.ms_img.controller;
 
 import com.bugtrackers.ms_img.services.CloudService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import java.util.UUID;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 @WebMvcTest(ImgController.class)
 public class ImgControllerTest {
