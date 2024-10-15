8e1e4ce879d83810fdc7efdaaa04453707cf7fff
Leví Hernández <levihernandez2501@gmail.com>
2024-10-15 17:16:34 -0600

    Fix: add children to routes, add missing curly brackets

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -5,27 +5,31 @@ import { HomeComponent } from './components/commons/home/home.component';
 import { LoginComponent } from './components/commons/login/login.component';
 import { NotFoundComponent } from './components/commons/not-found/not-found.component';
 import { CompanySettingsComponent } from './components/options/company-settings/company-settings.component';
-import {ProfileComponent} from "./components/user/profile/profile.component";
+import { ProfileComponent } from "./components/user/profile/profile.component";
 
 export const routes: Routes = [
 
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'test', component: TestComponentsComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'options/company-settings', component: CompanySettingsComponent},
-    {path: '**', component: NotFoundComponent}
-    {
-      path: 'edit', children: [
-        {path: 'profile', component: ProfileComponent}
-      ]
-    }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'test', component: TestComponentsComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'options', children: [
+      { path: 'company-settings', component: CompanySettingsComponent }
+    ]
+  },
+  {
+    path: 'edit', children: [
+      { path: 'profile', component: ProfileComponent }
+    ]
+  },
+  { path: '**', component: NotFoundComponent }
 
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
